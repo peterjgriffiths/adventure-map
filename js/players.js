cb69cba5380277f2d7be989ade1b3d4746b62a5c
peterjgriffiths <peterjgriff97@gmail.com>
2025-06-01 17:05:30 +0100

    added a hamburger menu, comments on a few files, and an early (non-working) leaderboard page

---
+++ js/players.js
@@ -0,0 +1,17 @@
+const players = [
+  {
+    name: "Aaron",
+    ownedLocations: ["Sydney Opera House", "Great Barrier Reef"],
+    visitedLocations: ["Eiffel Tower", "Statue of Liberty", "Great Barrier Reef"]
+  },
+  {
+    name: "Kieran",
+    ownedLocations: ["Eiffel Tower"],
+    visitedLocations: ["Sydney Opera House", "Statue of Liberty"]
+  },
+  {
+    name: "Leysh",
+    ownedLocations: ["Statue of Liberty"],
+    visitedLocations: ["Sydney Opera House", "Eiffel Tower", "Great Barrier Reef"]
+  }
+];
